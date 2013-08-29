21a9a8d3d313313111ce3c14ecb14d617591860f
peterwilliams97 <peter.williams.97#gmail.com>
2013-08-29 21:38:17 +1000

    better comments

--- n_queens.py
+++ n_queens.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-    Solve the N queens problem http://en.wikipedia.org/wiki/Eight_queens_puzzle
+    Solve the n queens problem http://en.wikipedia.org/wiki/Eight_queens_puzzle
     using naive constraint propagation.
     
     Usage: 
-        python n_queens.py <N>
+        python n_queens.py <n>
     e.g.
-        python .n_queens.py 20
+        python n_queens.py 20
         
     NOTES:
         1 Uses NumPy and Numba so the best way to run this is to get the latest Anaconda distro
           from https://store.continuum.io/
         2 If you don't want to use Numba then remove the @autojit. 
         3 If you don't even want to use Numpy then make board a list of lists.
-        4 This program only works for N with values up to about 900 (on Windows 64 bit) due to 
-          stack size limitations in python. If you want to solve for largee values of N then convert
+        4 This program only works for n with values up to about 900 (on Windows 64 bit) due to 
+          stack size limitations in python. If you want to solve for largee values of n then convert
           add_queen() from a recursive to an iterative function.
-        5 Alternative methods for solving N queens include
+        5 Alternative methods for solving n queens include
             Local search http://en.wikipedia.org/wiki/Min-conflicts_algorithm#Example
             MIP: http://scip.zib.de/download/files/scip_intro_01.pdf 
             A solver: e.g Comet: http://www.hakank.org/comet/queensn.co
@@ -28,22 +28,23 @@ import sys, random
 from numba import autojit
 
 
-def make_board(N):
-    """Create a N x N chessboard
-        Returns: An N X N integer NumPy array
+def make_board(n):
+    """Create a n x n chessboard
+        Returns: An n x n integer NumPy array
     """    
-    return np.zeros((N, N), dtype=np.int)
+    return np.zeros((n, n), dtype=np.int)
     
 
 @autojit
-def propagate(N, board, x, y, d):
+def propagate(n, board, x, y, d):
     """Propagate the constraints that arise from adding (d = +1) or removing (d = -1) a queen to the 
         board at (x, y)
         
-        N: Width of chessboard
+        n: Width of chessboard
         board: State of chessboard for current queens which does not include the one at (x, y). 
-               board[a, b] > 0 if (a, b) is threatened by any of the queens, == 0 otherwise
-               board will be updated to include the queen at (x, y) when this function returns
+               board[a, b] = number of queens threating (a, b)
+               board will be updated to include (d = +1) or exclude (d = -1) the queen at (x, y) 
+                when this function returns
         x, y: Coordinates of queen to be added/removed
         d: +1 to add a queen, -1 to remove a queen
     """
@@ -70,73 +71,76 @@ def propagate(N, board, x, y, d):
         x1 -= 1
         y1 -= 1    
 
-    while x2 < N and y2 >= 0:
+    while x2 < n and y2 >= 0:
         board[x2, y2] += d
         x2 += 1
         y2 -= 1
       
-    while x3 >= 0 and y3 < N:
+    while x3 >= 0 and y3 < n:
         board[x3, y3] += d
         x3 -= 1
         y3 += 1 
    
-    while x4 < N and y4 < N:
+    while x4 < n and y4 < n:
         board[x4, y4] += d
         x4 += 1
         y4 += 1     
 
 
-def add_queen(N, queens, board):
+def add_queen(n, queens, board):
     """Add a queen to the column to right of the queens that have been placed on the board so far.
         Queens are added from left column to right. 
         This function gets called recursively with a feasible list of queens and attempts to add one 
-        more queen immediately the right of those in the list.
+         more queen immediately the right of those in the list.
         
-        N: Width of chessboard
+        n: Width of chessboard
         queens: List of queens that have been placed so far. queens[i] = row of queen on column i
-        board: State of chessboard. board[x, y] = 1 if (x, y) is threatened by any of queens
-        Returns: Valid list of N queens if one has been found, None otherwise
+        board: State of chessboard. board[a, b] = number of queens threating (a, b)
+        Returns: Valid list of n queens if one has been found, None otherwise
         
         Note: board gets modified then restored to its state at entry to this function.
     """
-    # The queens are in feasible locations by design. If there are N of them so we must have a full 
+    # The queens are in feasible locations by design. If there are n of them so we must have a full 
     #  solution so we return it.
-    if len(queens) == N:
+    if len(queens) == n:
         return queens
 
     # Checking rows in random order makes this run faster. Why?
-    row_order = list(range(N))
+    row_order = list(range(n))
     random.shuffle(row_order) 
     
     x = len(queens)
     for y in row_order:
         # Don't check threatened positions. The efficiency of this program is basically due to the
-        #  pruning of the search space that results from this.
+        #  pruning of the search space that results from not checking positions threatened by the
+        #  existing queens.
         if board[x, y] != 0:
             continue
         
         # Add the contraints for the new queen at (x, y)
-        propagate(N, board, x, y, 1)
+        propagate(n, board, x, y, 1)
+        
         # Recurse
-        valid_queens = add_queen(N, queens + [y], board)
+        valid_queens = add_queen(n, queens + [y], board)
+        
         # Undo the constraints added for queen at x, y
-        propagate(N, board, x, y, -1)
+        propagate(n, board, x, y, -1)
         
         # If we found a feasible full board deeper in the recursion then return it.
         if valid_queens:
             return valid_queens
             
-    # If we got here then queens could be not be extended to a feasible N queens list 
+    # If we got here then queens could be not be extended to a feasible n queens list 
     return None
     
     
-def solve(N):
-    """Solve the N queens problem with constraint propagation
-        queens = solve(N) => queens[i] is the row of the queen in column i
-                           where 0 <= row < N and 0 <= column < N
+def solve(n):
+    """Solve the n queens problem using constraint propagation
+        queens = solve(n) => queens[i] is the row of the queen in column i
+                           where 0 <= row < n and 0 <= column < n
         Returns: List of rows for queens. 
     """
-    return add_queen(N, [], make_board(N))
+    return add_queen(n, [], make_board(n))
 
     
 if __name__ == '__main__':    
@@ -144,18 +148,21 @@ if __name__ == '__main__':
         print __doc__
         exit()
         
-    N = int(sys.argv[1])
-    print 'Solving %d queens problem' % N
+    n = int(sys.argv[1])
+    print 'Solving %d queens problem' % n
     
-    queens = solve(N)
+    # Make the result reproducible. 
+    random.seed(1337)
+    
+    queens = solve(n)
     
     print queens
     
-    if N > 80:
+    if n > 80:
         print 'Chessboard too big to display'
         exit()
         
     print '-' * 80
-    for y in range(N):
+    for y in range(n):
         print ''.join(' Q' if x == y else ' +' for x in queens) 
         
